9aeb1a70743f2782d6bee9b74f048e6d47fcfeda
HeadAndToes <55713950+HeadAndToes@users.noreply.github.com>
2021-11-25 14:52:16 +0100

    Add files via upload

--- liveFFT.py
+++ liveFFT.py
@@ -1,11 +1,11 @@
 import tkinter as tk
 import numpy as np
 import matplotlib.pyplot as plt
-
+from PIL import Image
 
 picW = 250
 picH = 250
-picSpace= np.ones((picW,picH))*0
+picSpace= np.ones((picW,picH))*255
 
 
 
@@ -23,14 +23,41 @@ def paint(event):
     # display the mouse movement inside canvas
     wn.create_oval(x1, y1, x2, y2, fill=color, outline=color)
     if (0 <= x1) and  (x1 < picW) and 0<=y1 and y1<picH:
-        picSpace[x1,y2]= 255
+        picSpace[x1,y2]= 0
     
 # create canvas
 wn=tk.Canvas(root, width=picW, height=picH, bg='white')
 # bind mouse event with canvas(wn)
 wn.bind('<B1-Motion>', paint)
 wn.pack()
+
+
+# show image
+image1 = Image.open("SpaceImage.png")
+test = tk.ImageTk.PhotoImage(image1)
+
+label1 = tk.tkinter.Label(image=test)
+label1.image = test
+
+# Position image
+label1.place(x=100, y=100)
 root.mainloop()
+
+
+root.mainloop()
+
+
+im = Image.fromarray(picSpace)
+
+if im.mode != 'RGB':
+    im = im.convert('RGB')
+im.save("SpaceImage.png")
 
 print("closed app")
 plt.imshow(picSpace, cmap='gray')
+
+
+
+
+
+
